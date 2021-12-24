cfa6808eea6ca249784ce9a3832eba0c0ce140fd
Javier Antonio <95066889+Javieranto56@users.noreply.github.com>
2021-12-23 21:04:03 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+  AOS.init();
+
+
+//menu
+
+let enlacesHeader = document.querySelectorAll(".enlaces-header")[0];
+let semaforo = true;
+
+document.querySelectorAll(".hamburguer")[0].addEventListener("click",function(){
+
+    if(semaforo){
+        document.querySelectorAll(".hamburguer")[0].style.color ="#fff";
+        semaforo=false;
+    }else{
+        document.querySelectorAll(".hamburguer")[0].style.color ="#000";
+        semaforo=true;
+    }
+    enlacesHeader.classList.toggle("menudos")
+})
